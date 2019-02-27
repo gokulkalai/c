624a7606cd5da01042cc160a33ede8fe8b97a350
gokulkalai <47973758+gokulkalai@users.noreply.github.com>
2019-02-27 15:53:10 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,16 @@
+#include<stido.h>
+int main()
+{
+int a;
+printf("enter the value:");
+scanf("%d,&a");
+if(a%2==0)
+{
+printf("a is even");
+}
+else
+{
+printf(" given number is odd");
+}
+return 0;
+}
