f70997ef124f9fe4cfb4d5a351f5b38d1ac5a8c1
gokulkalai <47973758+gokulkalai@users.noreply.github.com>
2019-02-27 16:25:56 +0530

    Create vowels.andconso.c

---
+++ vowels.andconso.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i;
+char a[100];
+printf("enter the string:");
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='I')
+{
+printf("vowels");
+}
+else
+{
+printf("consonents");
+}
+return o;
+}
