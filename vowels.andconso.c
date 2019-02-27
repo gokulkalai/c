cf2e62e9d51799e6e7e377fdd3981f2ce283f526
gokulkalai <47973758+gokulkalai@users.noreply.github.com>
2019-02-27 16:30:55 +0530

    Update vowels.andconso.c

--- vowels.andconso.c
+++ vowels.andconso.c
@@ -4,7 +4,7 @@ int main()
 int i;
 char a[100];
 printf("enter the string:");
-if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='I')
+if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='I')
 {
 printf("vowels");
 }
