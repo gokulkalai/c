558c16403ab28b21a52e28f3f54f9fea26487fd4
gokulkalai <47973758+gokulkalai@users.noreply.github.com>
2019-03-15 16:07:24 +0530

    Create STING.C

---
+++ STING.C
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void main()
+{
+char a[10],b[10];
+int k=0,l=o,i,j,c1=0,c2=0;
+gets(a);
+gets(b);
+for(i=0;a[i]!='\0';i++)
+{
+c1++;
+}
+for(i=0;b[i]!='\0';i++)
+{
+c2++;
+}
+if(c1==c2)
+{
+for(i=0;i<c1;i++)
+{
+if(a[i]==a[i+1])
+{
+a[i]=''$';
+}
+if(b[i]==b[i+1])
+{
+b[i]='$';
+}
+}
+for(i=0;i<c1;i++)
+{
+if(a[i]=='$')
+k++;
+if(b[i]=='$')
+1++;
+}
+if((k!=0)&&(1!=0))
+printf("yes");
+else
+printf("no");
+}
+else
+printf("no");
+getch();
+}
